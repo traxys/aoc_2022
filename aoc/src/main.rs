a49c2aca04be6a8c2c862c6807b38310615fbf67
traxys <quentin+dev@familleboyer.net>
2022-12-02 00:05:33 +0100

    Use Local time instead of Utc

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::OpenOptions, io::Write, os::unix::process::CommandExt, path::Path};
 
 use bstr::ByteSlice;
-use chrono::{Datelike, Utc};
+use chrono::{Datelike, Local};
 use clap::Parser;
 use color_eyre::eyre;
 use reqwest::header::{self, HeaderValue};
@@ -56,7 +56,7 @@ fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
     let args = Args::parse();
-    let time = Utc::now();
+    let time = Local::now();
     let day = args.day.unwrap_or_else(|| time.day());
 
     let workspace = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
