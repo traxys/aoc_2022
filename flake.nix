f959a8bcec778eba684f42fc43f65ca8ce0ed483
traxys <quentin+dev@familleboyer.net>
2022-12-02 00:04:20 +0100

    Add flake file

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.naersk.url = "github:nix-community/naersk";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+      rust = pkgs.rust-bin.stable.latest.default;
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [rust];
+        RUST_PATH = "${rust}";
+        shellHook = ''
+          alias rstddoc="firefox ${rust}/share/doc/rust/html/std/index.html"
+        '';
+      };
+    });
+}
